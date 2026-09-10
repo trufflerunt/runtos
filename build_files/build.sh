379d57372862a9e442632d376daf219f0f59416d
Truffle Runt <2749553+trufflerunt@users.noreply.github.com>
2026-09-10 15:26:14 +0200

    Removed redundant install packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,8 +13,9 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y akmod-nvidia-open libva-nvidia-driver xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
-dnf5 install -y tmux gdm niri
+#dnf5 install -y akmod-nvidia-open libva-nvidia-driver xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
+
+dnf5 install -y niri
 
 # Use a COPR Example:
 #
